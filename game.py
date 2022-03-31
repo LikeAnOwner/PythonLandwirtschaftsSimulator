3f6a54f22dd73dc5fb0db52834f3fe3faf14ae09
LikeAnOwner <LikeAnOwner@gmail.com>
2022-03-31 23:19:01 +0200

    add stuff without ole :(

--- game.py
+++ game.py
@@ -1,34 +1,50 @@
+import imp
 from rich import print
 from commands import getCommands
 import json
+from helper import *
 
-with open('default_data.json', 'r') as json_file:
-    default_data = json.load(json_file)
 
-with open('game_data.json', 'w') as json_file:
-        json.dump(default_data, json_file)
+name = ''
+if not isGameNew():
+    loadGame = input('Es wurde ein Spielstand gefunden. Möchtest du den Spielstand laden? (j/n)' + '\n> ')
 
-with open('game_data.json', 'r') as json_file:
-    game_data = json.load(json_file)
+    while loadGame != 'j' or loadGame != 'n':
+        if loadGame == 'j':
+            break
+        elif loadGame == 'n':
+            resetGameData()
+            print('Bitte gib deinen Namen ein.')
+            name = input('> ')
+            break
+        else:
+            print('[red]Bitte gib eine gültige Antwort ein![/red]')
+            loadGame = input('Es wurde ein Spielstand gefunden. Möchtest du den Spielstand laden? (j/n)' + '\n> ')
+else:
+    print('Bitte gib deinen Namen ein.')
+    name = input('> ')
+    resetGameData()
+
+game_data = loadGameData()
 
 def main():
-    print('Bitte gebe deinen Namen ein.')
-    game_data['name'] = input('> ')
-
-    if game_data['name'] == 'Pant':
+    if name == 'Pant':
         print('[chartreuse4]      _\______[/chartreuse4]')
         print('[chartreuse4]      /        \========[/chartreuse4]')
         print('[chartreuse4] ____|__________\_____[/chartreuse4]')
         print('[chartreuse4]/ ___________________ \\ [/chartreuse4]')
         print('[chartreuse4]\/ _===============_ \/[/chartreuse4]')
-        print('[chartreuse4]  "-===============-"[/chartreuse4]')
-
-    print('\n[chartreuse4]Herzlich Willkommen ' + game_data['name'] + ' zum Python Farming Simulator![/chartreuse4]', ':tractor:')
-    print('\nDu hast den alten Hof deines Großvaters geerbt. Leider sehen die Bilanzen gar nicht gut aus...')
-    print('\nVerwende den Befehl \'hilfe\', um eine Auflistung aller aktuell zur Verfügung stehenden Befehle zu erhalten.\n')
-
-    with open('game_data.json', 'w') as json_file:
-        json.dump(game_data, json_file)
+        print('[chartreuse4]  "-===============-"[/chartreuse4]\n')
+        print('\n[red]Sie sind mit Ihrem Panzer über den Hof gefahren und haben dabei alles zerstört...[/red]')
+        print('\n[red]Game Over[/red]')
+        exit()
+    elif name == '':
+        game_data['name'] = name
+        print('\n[chartreuse4]Herzlich Willkommen ' + game_data['name'] + ' zum Python Farming Simulator![/chartreuse4]', ':tractor:')
+        print('\nDu hast den alten Hof deines Großvaters geerbt. Leider sehen die Bilanzen gar nicht gut aus...')
+        print('\nVerwende den Befehl \'hilfe\', um eine Auflistung aller aktuell zur Verfügung stehenden Befehle zu erhalten.\n')
+    else:
+        print('Willkommen zurück ' + game_data['name'] + '!')
 
     while True:
         eingabe = input('> ')
---
+++ helper.py
@@ -0,0 +1,30 @@
+import json
+
+def isGameNew():
+    with open('game_data.json', 'r') as json_file:
+        game_data = json.load(json_file)
+
+    with open('default_data.json', 'r') as json_file:
+        default_data = json.load(json_file)
+
+    if game_data == default_data:
+        return True
+    else:
+        return False
+
+def resetGameData():
+    with open('default_data.json', 'r') as json_file:
+        default_data = json.load(json_file)
+
+    with open('game_data.json', 'w') as json_file:
+        json.dump(default_data, json_file)
+
+def loadGameData():
+    with open('game_data.json', 'r') as json_file:
+        game_data = json.load(json_file)
+
+    return game_data
+
+def saveGameData(game_data):
+    with open('game_data.json', 'w') as json_file:
+        json.dump(game_data, json_file)
